751c420d8ec8a190140ffc3cd6f4b6a2689dce60
Raza Ali <razaali2461357@gmail.com>
2025-09-04 12:11:13 -0700

    add custom logger using morgan and winston

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import logger from "./logger.js";
+import morgan from "morgan";
 dotenv.config({ path: "./.env" });
 const app = express();
 
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 app.use(express.json());
+
 let myCollection = [];
 let id = 1;
 
